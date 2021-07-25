0c0660b20a5884c9673c85bfd24b1d67d475131a
reflectecho <dan@reflectecho.com>
2021-07-25 11:31:37 -0700

    Removed lingering plugin remains and added a register page.

---
+++ register/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<title>Add Record</title>
+<link href="../style.css" rel="stylesheet" />
+</head>
+<body>
+<center><strong>You are about to register an account.</strong></center><br/>
+<table border="1" cellpadding="10" align="center">
+<form name="register" action="../registered/" method="post">
+<tr><td align="center">
+<input type="text" name="username" placeholder="Username?" />
+</td></tr>
+<tr><td align="center">
+<input type="password" name="password" placeholder="Password?" />
+</td></tr>
+<tr><td align="center"><input type="submit" value="Register Account!" /></td></tr>
+</form>
+</table>
+</body>
+</html>
