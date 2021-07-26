9bda3eaa3250023286e47cb44c8541b0609cfed2
reflectecho <dan@reflectecho.com>
2021-07-26 10:02:47 -0700

    Added login page and form.

---
+++ login/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<title>Login</title>
+<link href="../style.css" rel="stylesheet" />
+</head>
+<body>
+<center><strong>You are about to login to your account.</strong></center><br/>
+<table border="1" cellpadding="10" align="center">
+<form name="register" action="../registered/" method="post">
+<tr><td align="center">
+<input type="text" name="email" placeholder="Email?" />
+</td></tr>
+<tr><td align="center">
+<input type="password" name="password" placeholder="Password?" />
+</td></tr>
+<tr><td align="center"><input type="submit" value="Login!" /></td></tr>
+</form>
+</table>
+</body>
+</html>
