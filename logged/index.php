b44895f82e71fc39ce6cca01877747fdf7840de6
reflectecho <dan@reflectecho.com>
2021-08-01 16:41:16 -0700

    Notate tasks for future development.

--- logged/index.php
+++ logged/index.php
@@ -5,6 +5,16 @@
 <link href="../style.css" rel="stylesheet" />
 </head>
 <body>
+<!--
 
+Get form fields from login form.
+
+Select if account exists to compare passwords.
+
+If correct password add boolean variable set to true in a session.
+
+If incorrect password throw error.
+
+-->
 </body>
 </html>
