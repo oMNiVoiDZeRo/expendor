248b0118e68811b41125d9ba6a87c3d068230a75
reflectecho <dan@reflectecho.com>
2021-08-03 11:45:09 -0700

    Deleted a comment.

--- logged/index.php
+++ logged/index.php
@@ -14,8 +14,6 @@ $password = mysqli_real_escape_string($conn, $_POST['password']);
 ?>
 <!--
 
-Get form fields from login form.
-
 Select if account exists to compare passwords.
 
 If correct password add boolean variable set to true in a session.
