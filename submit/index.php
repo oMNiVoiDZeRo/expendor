f72c33aacf44a80347d9349b8512955cfd267be9
reflectecho <dan@reflectecho.com>
2021-08-09 16:45:18 -0700

    Removed another space.

--- submit/index.php
+++ submit/index.php
@@ -59,7 +59,7 @@ if($file != 'No file attached.' && file_exists($file)) {
 } else {
 		$fileDeleteMessage = "<br/><br/>No attachment found to delete.";
 }
-	
+
 $sql = "DELETE FROM `$username` WHERE `UID` = '$date'";
 if(mysqli_query($conn, $sql)){
 echo '<table border="1" cellpadding="10" align="center">';
